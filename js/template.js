99fd1e36a5fb08b9230a4adc6d6af3b1f8116e56
Joey <josfeldmann@gmail.com>
2018-08-13 02:06:34 -0400

    Title page format near complete need images

--- js/template.js
+++ js/template.js
@@ -1,14 +1,12 @@
 
 
 function makeNavbar(){
+	document.getElementById("navarea").innerHTML = document.getElementById("navarea").innerHTML = "<nav class='navbar navbar-inverse'> <a href='index.html' class='navbar-left'><img src='images/logo.png'></a><div class='container-fluid'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>Menu</button><a class='navbar-brand' href='index.html'>UMD GDC</a></div>      <div class='collapse navbar-collapse float_right' id='myNavbar'>        <ul class='nav navbar-nav'>       <li><a href='index.html'>Home</a></li>     <li><a href='events.html'>Events</a></li>     <li><a href='games.html'>Games</a></li>          <li><a href='learn.html'>Learn</a></li>          <li><a href='about.html'>About</a></li>          <li><a href='index.html#contact'>Contact</a></li></ul></div></div></nav>";
 
-
-	//<li><a href='resources.html'>Resources</a></li>   <li><a href='about.html'>About</a></li>          
-    document.getElementById("navarea").innerHTML = document.getElementById("navarea").innerHTML = "<nav class='navbar navbar-inverse'> <a href='index.html' class='navbar-left'><img src='images/logo.png'></a><div class='container-fluid'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>Menu</button><a class='navbar-brand' href='index.html'>UMD GDC</a></div>      <div class='collapse navbar-collapse float_right' id='myNavbar'>        <ul class='nav navbar-nav'>       <li><a href='index.html'>Home</a></li>     <li><a href='events.html'>Events</a></li>     <li><a href='games.html'>Games</a></li>          <li><a href='learn.html'>Learn</a></li>          <li><a href='about.html'>About</a></li>          <li><a href='index.html#contact'>Contact</a></li></ul></div></div></nav>";
-}
+	document.getElementsByTagName("head")[0].appendChild("<link rel='shortcut icon' type='image/x-icon' href='plswork.ico'>");
+	}
 
 function makeNavbarFolder(){
-
 	document.getElementById("navarea").innerHTML = document.getElementById("navarea").innerHTML = "<nav class='navbar navbar-inverse'> <a href='../index.html' class='navbar-left'><img src='../images/logo.png'></a><div class='container-fluid'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>Menu</button><a class='navbar-brand' href='../index.html'>UMD GDC</a></div>      <div class='collapse navbar-collapse float_right' id='myNavbar'>        <ul class='nav navbar-nav'>       <li><a href='../index.html'>Home</a></li>     <li><a href='../events.html'>Events</a></li>     <li><a href='../games.html'>Games</a></li>          <li><a href='../learn.html'>Learn</a></li>          <li><a href='../about.html'>About</a></li>          <li><a href='../index.html#contact'>Contact</a></li></ul></div></div></nav>";
 }
 
